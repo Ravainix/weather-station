76d523c11a56aebd86dd80de2d8a8f5a64a87aa3
Ravainix <ravainix@gmail.com>
2021-05-11 08:55:29 +0200

    fix model binding

--- server/Core/Models/measure.go
+++ server/Core/Models/measure.go
@@ -4,8 +4,8 @@ import "time"
 
 type Measure struct {
 	ID          int     `json:"id" gorm:"primary_key"`
-	Temperature float32 `json:"temperature"`
-	Humidity    int     `json:"humidity"`
+	Temperature float32 `json:"temperature,omitempty" binding:"required"`
+	Humidity    int     `json:"humidity,omitempty"`
 	Pressure    int     `json:"pressure"`
 	Battery     int     `json:"battery"`
 	CreatedAt   time.Time
